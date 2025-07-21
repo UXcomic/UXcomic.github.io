52ba2ac6482b458b367b1dbbd7c7a0fb6a7d9781
hungvuongg71 <hungvuongg71@gmail.com>
2025-07-21 15:09:53 +0700

    Config cloudinary cdn

--- src/app/components/notion-image-component/notion-image-component.ts
+++ src/app/components/notion-image-component/notion-image-component.ts
@@ -1,12 +1,12 @@
 import { AfterViewInit, Component, ElementRef, inject, Input, PLATFORM_ID, ViewChild } from '@angular/core'
 import { environment } from '../../../environments/environment'
 import { NotionTextComponent } from '../notion-text-component/notion-text-component'
-import { isPlatformBrowser, NgOptimizedImage } from '@angular/common'
+import { isPlatformBrowser } from '@angular/common'
 
 @Component({
   selector: 'app-notion-image-component',
   standalone: true,
-  imports: [NotionTextComponent, NgOptimizedImage],
+  imports: [NotionTextComponent],
   templateUrl: './notion-image-component.html',
   styleUrl: './notion-image-component.sass',
 })
