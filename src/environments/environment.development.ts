54ae9e4dbfe09144e51526254228abc81c489c08
hungvuongg71 <hungvuongg71@gmail.com>
2025-07-21 09:30:12 +0700

    Change CDN Links test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   baseUrl: 'http://localhost:4200',
+  cdnUrl: 'http://localhost:4200',
   aboutPostId: '10f5d164-78d8-804f-b8ea-e2a265424273',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   baseUrl: 'https://uxcomic.github.io',
+  cdnUrl: 'https://cdn.jsdelivr.net/gh/UXcomic/UXcomic.github.io@gh-pages',
   aboutPostId: '10f5d164-78d8-804f-b8ea-e2a265424273',
 }
