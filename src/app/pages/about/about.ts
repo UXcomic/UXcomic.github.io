d0afde7594980bf1dcfe8fe75e8645b351a53a0d
hungvuongg71 <hungvuongg71@gmail.com>
2025-08-07 06:04:37 +0000

    Add metadata about page

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -5,6 +5,7 @@ import { PostContent } from '../../models/post-content'
 import { environment } from '../../../environments/environment'
 import { convertPostContent } from '../../utils/post-helper'
 import { PostContentSection } from '../../sections/post-content-section/post-content-section'
+import { Meta, Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-about',
@@ -17,6 +18,8 @@ export class About implements OnInit {
   protected post?: PostContent
   protected config = environment
 
+  private meta = inject(Meta)
+  private title = inject(Title)
   private location = inject(Location)
 
   goBack() {
@@ -26,5 +29,29 @@ export class About implements OnInit {
   ngOnInit(): void {
     const data = (PostData as any[]).find((p: any) => p.id === this.config.aboutPostId)
     this.post = convertPostContent(data)
+
+    this.initTitle()
+    this.initMetaTags()
+  }
+
+  private initTitle() {
+    this.title.setTitle(this.generateTitle() + ' | ' + this.config.prefixOGTitle)
+  }
+
+  private initMetaTags() {
+    this.updateOGTitle()
+  }
+
+  private updateOGTitle() {
+    this.meta.updateTag({
+      name: 'og:title',
+      content: this.generateTitle() + ' | ' + this.config.prefixOGTitle,
+    })
+  }
+
+  private generateTitle() {
+    let name = this.title.getTitle() || this.meta.getTag('name="og:title"')?.content.trim() || ''
+    if (this.post?.title) name = this.post.title
+    return name
   }
 }
